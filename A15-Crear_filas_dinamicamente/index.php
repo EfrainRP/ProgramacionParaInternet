7408ff7b85c58bd08d6e44b628cfc647d8f8c870
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-03-18 10:11:10 -0600

    Finished A15

--- A15-Crear_filas_dinamicamente/index.php
+++ A15-Crear_filas_dinamicamente/index.php
@@ -2,19 +2,16 @@
     <head>
         <title>A15. Crear filas dinamicamente</title>
         <link href="./style.css" rel="stylesheet" type="text/css">
-        <!-- <script src="./JS/jquery-3.3.1.min.js"></script> -->
         <script>
             function getFila(){
                 var filas = document.forma01.filas.value;
                 console.log(filas);
                 if(filas == 0){
                     alert("Valor invalido...");
-                    return false;
                 }
                 else{
                     console.log(filas);
-                    // alert(filas);
-                    document.form01.submit();
+                    document.form01.submit(); //Se ejecuta el metodo submit para enviar los datos al formulario
                 }
             }
         </script>
@@ -23,12 +20,11 @@
         <h1>Crear filas dinamicamente</h1>
         <br> 
         <form name="forma01" action="./recibe.php" method="POST">
-            <label for="Lfilas">Filas:</label>
+            <label for="filas">Filas:</label>
             <select id="filas" name="filas">
-                <option value="0" selected>Selecciona</option>;
+                <option value="0" selected>Selecciona una opcion</option>; <!--Opcion ya seleccionada-->
                 <?php
-                    // $selectNum = 5000;
-                    for($i=1;$i<5000;$i++){
+                    for($i=1;$i<=5000;$i++){
                         echo "<option value=\"$i\">".$i."</option>";
                     }
                 ?>
--- A15-Crear_filas_dinamicamente/recibe.php
+++ A15-Crear_filas_dinamicamente/recibe.php
@@ -1,13 +1,14 @@
 <?php
     echo '<link href="./style.css" rel="stylesheet" type="text/css">'; //Se hace referencia al css para tener el estilo
     
-    echo '<a href="./formularioFilas.php">Volver</a>';
+    echo '<a href="./index.php">Volver</a><br><br>'; //Boton de regreso al formulario
 
     $filas = $_POST['filas'];
-
-    echo "<table class=\"tabla\">";
-    for($i=1;$i<=$filas;$i++){
-        echo "<tr><td>$i</td></tr>";
+    if($filas>0){ //Si es mayor a 0, generara la tabla con sus numero de fila
+        echo "<table class=\"tabla\">";
+        for($i=1;$i<=$filas;$i++){
+            echo "<tr><td>$i</td></tr>";
+        }
+        echo"</table>";
     }
-    echo"</table>";
 ?>
