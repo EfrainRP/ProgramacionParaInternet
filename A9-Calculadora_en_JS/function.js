485c3b5b2058b816285b03580fef9e31f9a5e260
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-03-26 20:50:24 -0600

    completing calcualtor

--- A9-Calculadora_en_JS/function.js
+++ A9-Calculadora_en_JS/function.js
@@ -19,22 +19,28 @@ let operator = '';
 let display = $('#display');
 
 function numberClicked(number) {
-
-    console.log("op1: "+ op1);
-    console.log("op2: "+ op2);
-    console.log("op: "+ operator);
-    console.log("r: "+ result);
- 
-    display.val(display.val() + number);
+    if(result == display.val()){
+        display.val('');
+    }
+        display.val(display.val() + number);
+        // console.log("op1: "+ op1);
+        // console.log("op2: "+ op2);
+        // console.log("op: "+ operator);
+        // console.log("r: "+ result);
+        // console.log("display: "+ display.val());
 
 }
 
-function clearDisplay() {
-    display.val('');
-    op1 = '';
-    op2= '';
-    result = '';
-    operator = '';
+function clearDisplay(text) {
+    if(text=='AC'){
+        display.val('');
+        op1 = '';
+        op2= '';
+        result = '';
+        operator = '';
+    }else{
+        display.val('');
+    }
 }
 
 function operation(op) {
@@ -45,28 +51,35 @@ function operation(op) {
 
 function calculate() {
     op2 = display.val();
-    console.log("op1: "+ op1);
-    console.log("op2: "+ op2);
-    console.log("op: "+ operator);
-
+    
     switch(operator) {
         case '+':
             result = parseFloat(op1) + parseFloat(op2);
             break;
         case '-':
             result = parseFloat(op1) - parseFloat(op2);
+            result = result.toFixed(4); //Se redondea, debido a un error de calculo por los flotantes en js
             break;
         case '*':
             result = parseFloat(op1) * parseFloat(op2);
             break;
         case '/':
-            if (op2 === '0'){
+            if (op2 == '0'){
                 result = 'Error';
             }else{
                 result = parseFloat(op1) / parseFloat(op2);
             }
             break;
+        default:
+            result = "Error"
+            break;
     }
-    console.log(result);
-    display.val(result);
+    operator = ''; //Limpiamos el operador para futuras operaciones
+    display.val(result.toString());
+    console.log("op1: "+ op1);
+    console.log("op2: "+ op2);
+    // console.log("op: "+ operator);
+    console.log("r: "+ result);
+    // console.log("display: "+ display.val());
+
 }
