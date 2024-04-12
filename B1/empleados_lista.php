d52f06ada672c4970437b48c9b10054edb7da6b2
EFRA Mac <efrainroblespulido@gmail.com>
2024-04-12 15:31:24 -0600

    B1 finished

--- B1/empleados_lista.php
+++ B1/empleados_lista.php
@@ -1,11 +1,14 @@
+<?php //Conexion y verificacion de la base de datos
+    require "funciones/conecta.php";
+    $con = conecta();//conecta y verifica si se hizo bien
+?>
 <html>
     <head>
         <title>Lista de empleados</title>
-        <?php //Conexion y verificacion de la base de datos
-            require "funciones/conecta.php";
-            $con = conecta();//conecta y verifica si se hizo bien
-        ?>
-        <link href="./style.css" rel="stylesheet" type="text/css">
+        <link href="./style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css">
+        <!-- evitar la caché del navegador. Genera una cadena de consulta con el tiempo actual 
+        (time()) como un parámetro, lo que hace que el navegador considere cada solicitud como 
+        única y no cargue la versión almacenada en caché del archivo CSS si ha cambiado.-->
     </head>
     <body>
         <?php
@@ -15,16 +18,17 @@
 $res = $con->query($sql);//ejecuta una consulta en la conexion
 $num = $res->num_rows;
 
-echo "<h1>Lista de empleados ($num)</h1><br>";
+echo "<br><h1>Lista de empleados ($num)</h1>";
 ?>
-            <a href="./empleados_alta.php">Crear nuevo registro</a><br><br>
+            <a id="crearRegistro" href="./empleados_alta.php">Crear nuevo registro</a><br><br>
+            <br>
             <div class="table">
                 <div class='header' id="id"><b>ID</b></div>
                 <div class='header' id="nombre"><b>Nombre</b></div>
                 <div class='header' id="apellidos"><b>Apellidos</b></div>
                 <div class='header' id="correo"><b>Correos</b></div>
                 <div class='header' id="rol"><b>Roles</b></div>
-                <div class="header" id="opciones"><b>Opiones</b></div>
+                <div class="header" id="opciones"><b>Opciones</b></div>
                 
                 <?php
                     while($row = $res->fetch_array()){
@@ -32,17 +36,17 @@ echo "<h1>Lista de empleados ($num)</h1><br>";
                         $nombre = $row["nombre"];
                         $apellidos = $row["apellidos"];
                         $correo = $row["correo"];
-                        $rol = $row["rol"];
+                        $rol = $row["rol"] == 1?"Ejecutivo":"Gerente";
 
-                        echo "<div class='info'             id='id'>$id</div>
+                        echo "<div class='info' id='$id'>$id</div>
                             <div class='info' id='nombre'>$nombre</div>
                             <div class='info' id='apellidos'>$apellidos</div>
                             <div class='info' id='correro'>$correo</div>
                             <div class='info' id='rol'>$rol</div>
-                            <div class='info' id='detalles'>Ver detalles</div>
-                            <div class='info' id='editar'>Editar</div>
-                            <div class='info' id='eliminar'>Eliminar</div>";
-                        // echo "$id $nombre $apellidos $correo <br>";
+                            <a class='opciones' id='detalles' href='detalles.com'>Ver detalles</a>
+                            <a class='opciones' id='editar' href='editar.com'>Editar</a>
+                            <a class='opciones' id='eliminar' href='eliminar.com'>Eliminar</a>";
+                            // Los href's se cambiara en un futuro, es provicional
                     }
                 ?>
             </div>
---
+++ sql/d02.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 12, 2024 at 11:28 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `d02`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empleados`
+--
+
+CREATE TABLE `empleados` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(128) NOT NULL,
+  `apellidos` varchar(128) NOT NULL,
+  `correo` varchar(128) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  `rol` int(1) NOT NULL,
+  `archivo_n` varchar(255) NOT NULL,
+  `archivo` varchar(128) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 1,
+  `eliminado` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `empleados`
+--
+
+INSERT INTO `empleados` (`id`, `nombre`, `apellidos`, `correo`, `pass`, `rol`, `archivo_n`, `archivo`, `status`, `eliminado`) VALUES
+(1, 'Efrain', 'Robles Pulido', 'efrain.robles@gmail.com', 'Efra123', 1, '', '', 1, 0),
+(2, 'Juan', 'Gutierrez Ramos', 'marco@gmail.com', 'Abc123', 0, '', '', 1, 0),
+(3, 'Miguel', 'Gonzales Gallo', 'miguel@mail.com', 'miguel123', 0, '', '', 1, 0),
+(4, 'Marco', 'Polo Madrid', 'marco@gmail.com', 'marco456', 1, '', '', 1, 0),
+(5, 'Andrea', 'Perez Rubio', 'andrea@alumnos.udg', 'andre789', 0, '', '', 1, 0),
+(6, 'Saul', 'Palacios Villa', 'saul@mail.mx', 'saul012', 1, '', '', 1, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
