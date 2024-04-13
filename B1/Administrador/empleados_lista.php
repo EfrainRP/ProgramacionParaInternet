903275a634690aff75db71582cd54a9ab8e37510
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-04-13 15:24:02 -0600

    Finished B1

--- B1/Administrador/empleados_lista.php
+++ B1/Administrador/empleados_lista.php
@@ -4,25 +4,26 @@
 ?>
 <html>
     <head>
-        <title>Lista de empleados</title>
-        <link href=".\css\style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css">
-        <!-- evitar la caché del navegador. Genera una cadena de consulta con el tiempo actual 
+        <title>Listado de empleados</title>
+        <link href="./css/style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css">
+        <!-- El ?v=.... evitar la caché del navegador. Genera una cadena de consulta con el tiempo actual 
         (time()) como un parámetro, lo que hace que el navegador considere cada solicitud como 
         única y no cargue la versión almacenada en caché del archivo CSS si ha cambiado.-->
     </head>
     <body>
-        <?php
+        <?php //Consulta a la base de datos de empleados activos y no eliminados
             $sql = "SELECT * FROM empleados
                     WHERE status = 1 AND eliminado = 0";
 
-            $res = $con->query($sql);//ejecuta una consulta en la conexion
+            $res = $con->query($sql); //ejecuta una consulta en la conexion
             $num = $res->num_rows;
 
-            echo "<br><h1>Lista de empleados ($num)</h1>";
+            echo "<br><h1>Listado de empleados ($num)</h1>";
         ?>
-        <a id="crearRegistro" href="./empleados_alta.php">Crear nuevo registro</a><br><br>
+        <a class='opciones' id="crearRegistro" href="./empleados_alta.php">Crear nuevo registro</a><br><br>
         <br>
         <div class="table">
+            <!-- Header de la tabla -->
             <div class='header' id="id"><b>ID</b></div>
             <div class='header' id="nombre"><b>Nombre</b></div>
             <div class='header' id="apellidos"><b>Apellidos</b></div>
@@ -31,13 +32,16 @@
             <div class="header" id="opciones"><b>Opciones</b></div>
             
             <?php
-                while($row = $res->fetch_array()){
+                /*fetch_array nos recupera una fila de resultados 
+                como una matriz asociativa de la consulta*/
+                while($row = $res->fetch_array()){ 
                     $id = $row["id"];
                     $nombre = $row["nombre"];
                     $apellidos = $row["apellidos"];
                     $correo = $row["correo"];
                     $rol = $row["rol"] == 1?"Ejecutivo":"Gerente";
 
+                    // Datos obtenidos de la consulta
                     echo "<div class='info' id='$id'>$id</div>
                         <div class='info' id='nombre'>$nombre</div>
                         <div class='info' id='apellidos'>$apellidos</div>
--- B1/Administrador/func/conecta.php
+++ B1/Administrador/func/conecta.php
@@ -1,11 +1,12 @@
 <?php
+    //Definicion de parametros para la BD
     define("HOST",'localhost');
     define("BD",'d02');
     define("USER_BD",'root');
     define("PASS_BD",'');
     
-    function conecta(){
-        $con = new mysqli(HOST, USER_BD, PASS_BD, BD);
+    function conecta(){ //Funcion para la conexion a la BD de mysql
+        $con = new mysqli(HOST, USER_BD, PASS_BD, BD); 
         return $con;
     }
 ?>
