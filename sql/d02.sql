ead64a55584d722792faa1900ccf51aec35ed85d
EFRA Mac <efrainroblespulido@gmail.com>
2024-04-30 07:30:21 -0600

    DB products and promo

--- sql/d02.sql
+++ sql/d02.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 25-04-2024 a las 07:07:58
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Host: localhost
+-- Generation Time: Apr 30, 2024 at 03:27 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `d02`
+-- Database: `d02`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `empleados`
+-- Table structure for table `empleados`
 --
 
 CREATE TABLE `empleados` (
@@ -41,40 +41,96 @@ CREATE TABLE `empleados` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `empleados`
+-- Dumping data for table `empleados`
 --
 
 INSERT INTO `empleados` (`id`, `nombre`, `apellidos`, `correo`, `pass`, `rol`, `archivo_n`, `archivo`, `status`, `eliminado`) VALUES
-(1, 'Efrain', 'Robles Pulido', 'efrain.robles@gmail.com', '202cb962ac59075b964b07152d234b70', 1, 'efra.png', '99e3524a2987766ac65a1ec10ab52ceb.png', 1, 0),
-(2, 'Pedro', 'Lomeli Gaspar', 'pedro123@mail.com', 'd3ce9efea6244baa7bf718f12dd0c331', 1, '', '', 1, 0),
+(1, 'Efrain', 'Robles Pulido', 'efrain.robles@gmail.com', '668959847cdc4461e7253b5af2b5856e', 1, '', '', 1, 0),
+(2, 'Juan', 'Gutierrez Ramos', 'juan@gmail.com', 'Abc123', 2, '', '', 1, 0),
 (3, 'Miguel', 'Gonzales Gallo', 'miguel@mail.com', 'miguel123', 2, '', '', 1, 0),
 (4, 'Marco', 'Polo Madrid', 'marco@gmail.com', 'marco456', 1, '', '', 1, 0),
 (5, 'Andrea', 'Perez Rubio', 'andrea@alumnos.udg', 'andre789', 2, '', '', 1, 0),
 (6, 'Saul', 'Palacios Villa', 'saul@mail.mx', 'saul012', 1, '', '', 1, 0),
 (7, 'Oliver', 'Sanchez', 'oliver@mail.com', 'e99a18c428cb38d5f260853678922e03', 2, '', '', 1, 0),
 (8, 'Estefania', 'Lopez', 'lopez@mail.com', 'f9ffc9631679d3ff03da5a93cfcf1433', 1, '', '', 1, 0),
-(9, 'Pepe', 'Gomez Diaz', 'pepe@mail.com', '5e783f68bbe280088d77a82cbd235a0d', 1, '', '', 1, 0),
-(10, 'Lola', 'Arias Dominguez', 'paola@mail.com', 'e3c7ea9fe0fe7b6cca63a58d4ce990b0', 2, 'Alicia.png', '3844edcac1d74d900cf0fa30d56764f5.png', 1, 0);
+(9, 'Pepe', 'Gomez Diaz', 'pepe@mail.com', '5e783f68bbe280088d77a82cbd235a0d', 1, '', '', 1, 0);
+
+-- --------------------------------------------------------
 
 --
--- Índices para tablas volcadas
+-- Table structure for table `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(128) NOT NULL,
+  `codigo` varchar(32) NOT NULL,
+  `descripcion` text NOT NULL,
+  `costo` double NOT NULL,
+  `stock` int(11) NOT NULL,
+  `archivo_n` varchar(255) NOT NULL,
+  `archivo` varchar(128) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 1,
+  `eliminado` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promociones`
+--
+
+CREATE TABLE `promociones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(128) NOT NULL,
+  `archivo` varchar(64) NOT NULL,
+  `status` int(1) NOT NULL,
+  `eliminado` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `empleados`
+-- Indexes for table `empleados`
 --
 ALTER TABLE `empleados`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indexes for table `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `promociones`
+--
+ALTER TABLE `promociones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `empleados`
+-- AUTO_INCREMENT for table `empleados`
 --
 ALTER TABLE `empleados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `promociones`
+--
+ALTER TABLE `promociones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
