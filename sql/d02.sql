64dfaddc1f8a4c66cea0649c02c1170ace0aa02e
EFRA Mac <efrainroblespulido@gmail.com>
2024-05-10 21:27:35 -0600

    sql update

--- sql/d02.sql
+++ sql/d02.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 09, 2024 at 04:59 AM
+-- Generation Time: May 11, 2024 at 05:26 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -45,15 +45,15 @@ CREATE TABLE `empleados` (
 --
 
 INSERT INTO `empleados` (`id`, `nombre`, `apellidos`, `correo`, `pass`, `rol`, `archivo_n`, `archivo`, `status`, `eliminado`) VALUES
-(1, 'Efrain', 'Robles Pulido', 'efrain.robles@gmail.com', '902e756b0329c9f938111643ae5f1425', 1, '', '', 1, 0),
+(1, 'Efrain', 'Robles Pulido', 'efrain.robles@gmail.com', '902e756b0329c9f938111643ae5f1425', 1, 'Efrain.png', '99e3524a2987766ac65a1ec10ab52ceb.png', 1, 0),
 (2, 'Juan', 'Gutierrez Ramos', 'juan@gmail.com', 'Abc123', 2, '', '', 1, 0),
 (3, 'Miguel', 'Gonzales Gallo', 'miguel@mail.com', 'miguel123', 2, '', '', 1, 0),
 (4, 'Marco', 'Polo Madrid', 'marco@gmail.com', 'marco456', 1, '', '', 1, 0),
-(5, 'Andrea', 'Perez Rubio', 'andrea@alumnos.udg', 'andre789', 2, '', '', 1, 0),
+(5, 'Andrea', 'Perez Rubio', 'andrea@alumnos.udg', 'andre789', 2, 'Andrea.png', '66b013bd0d4f0de371b32fff5639c499.png', 1, 0),
 (6, 'Saul', 'Palacios Villa', 'saul@mail.mx', 'saul012', 1, '', '', 1, 0),
 (7, 'Oliver', 'Sanchez', 'oliver@mail.com', 'e99a18c428cb38d5f260853678922e03', 2, '', '', 1, 0),
-(8, 'Estefania', 'Lopez', 'lopez@mail.com', 'f9ffc9631679d3ff03da5a93cfcf1433', 1, '', '', 1, 0),
-(9, 'Pepe', 'Gomez Diaz', 'pepe@mail.com', '5e783f68bbe280088d77a82cbd235a0d', 1, '', '', 1, 0);
+(8, 'Estefania', 'Lopez', 'lopez@mail.com', 'f9ffc9631679d3ff03da5a93cfcf1433', 1, 'Estefania.png', '3844edcac1d74d900cf0fa30d56764f5.png', 1, 0),
+(9, 'Pepe', 'Gomez Diaz', 'pepe@mail.com', '5e783f68bbe280088d77a82cbd235a0d', 1, 'Pepe.png', 'fa01e38d3a6e74a79175ae8152fb30f6.png', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -68,10 +68,18 @@ CREATE TABLE `productos` (
   `descripcion` text NOT NULL,
   `costo` double NOT NULL,
   `stock` int(11) NOT NULL,
+  `archivo_n` varchar(255) NOT NULL,
   `archivo` varchar(128) NOT NULL,
   `status` int(1) NOT NULL DEFAULT 1,
   `eliminado` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `codigo`, `descripcion`, `costo`, `stock`, `archivo_n`, `archivo`, `status`, `eliminado`) VALUES
+(16, 'iphone', '12345', 'Celular', 342.234, 3, 'MT223.jpeg', '1be7dcf08c7e94034fdaffde8f34a003.jpeg', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -83,9 +91,17 @@ CREATE TABLE `promociones` (
   `id` int(11) NOT NULL,
   `nombre` varchar(128) NOT NULL,
   `archivo` varchar(64) NOT NULL,
-  `status` int(1) NOT NULL,
-  `eliminado` int(1) NOT NULL
+  `status` int(1) NOT NULL DEFAULT 1,
+  `eliminado` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `promociones`
+--
+
+INSERT INTO `promociones` (`id`, `nombre`, `archivo`, `status`, `eliminado`) VALUES
+(1, '2x1 4345gh', 'fa01e38d3a6e74a79175ae8152fb30f6.png', 1, 1),
+(2, 'inakjnkmnlm', 'fa01e38d3a6e74a79175ae8152fb30f6.png', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -123,13 +139,13 @@ ALTER TABLE `empleados`
 -- AUTO_INCREMENT for table `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `promociones`
 --
 ALTER TABLE `promociones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
